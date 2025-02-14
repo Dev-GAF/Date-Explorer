5c7ceb655af2d7f066a0ba2dc55e90d233eb196a
Guilherme A. Fortes <guilhermefortesoffice@gmail.com>
2025-02-14 13:01:43 -0300

    Update Menu.java
    Bug de indentação

--- menu/Menu.java
+++ menu/Menu.java
@@ -5,11 +5,12 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 //import java.io.*;
 
-import calendario.Data;
+import calendario.Data; 
+
 public class Menu {
     
-    private BufferedReader entrada;
-    private Data data;
+	private BufferedReader entrada;
+	private Data data;
 
 	public Data validarData (Object data) throws IllegalArgumentException
 	{
@@ -22,61 +23,61 @@ public class Menu {
 		return (Data)data;
 	}
 
-    public Menu (Data data) throws IllegalArgumentException
-    {
-        this.data    = this.validarData(data);
-        this.entrada = new BufferedReader(new InputStreamReader(System.in));
-    } 
+	public Menu (Data data) throws IllegalArgumentException
+	{
+		this.data    = this.validarData(data);
+		this.entrada = new BufferedReader(new InputStreamReader(System.in));
+	} 
 
-    public void exibirMenu () 
-    {
-        try
+	public void exibirMenu () 
+	{
+		try
 		{
 			while(true) 
-            {
+	    		{
 				System.out.println("\nEscolha uma opção:");
-                System.out.println("(1) - Avançar um dia");
-                System.out.println("(2) - Avançar vários dias");
-                System.out.println("(3) - Retroceder um dia");
-                System.out.println("(4) - Retroceder vários dias");
-                System.out.println("(5) - Obter o dia seguinte");
-                System.out.println("(6) - Obter vários dias adiante");
-                System.out.println("(7) - Obter o dia anterior");
-                System.out.println("(8) - Obter vários dias atrás");
-                System.out.println("(0) - Sair");
+				System.out.println("(1) - Avançar um dia");
+				System.out.println("(2) - Avançar vários dias");
+				System.out.println("(3) - Retroceder um dia");
+				System.out.println("(4) - Retroceder vários dias");
+				System.out.println("(5) - Obter o dia seguinte");
+				System.out.println("(6) - Obter vários dias adiante");
+				System.out.println("(7) - Obter o dia anterior");
+				System.out.println("(8) - Obter vários dias atrás");
+				System.out.println("(0) - Sair");
 				System.out.print("\nDigite uma opção: ");
-
+	
 				byte opcao = Byte.parseByte(entrada.readLine());
-
+	
 				if(opcao==0)
 					break;
 				
 				switch(opcao) 
-                {
+				{
 					case 1:
 						
-
+	
 					case 2:
 						
 						
 					case 3:
-                        
-
+			
+	
 					case 4:
 						
-
+	
 					case 5:
 						
-
+	
 					case 6:
 						
-
+	
 					case 7:
 						
-
+	
 					case 8:
 						
-
+	
 					default:
 						System.err.println("Opção inválida!");
 						break;
@@ -92,5 +93,5 @@ public class Menu {
 		{
 			System.err.println("Erro inesperado: "+ e.getMessage());
 		}
-    }
+	}
 }
